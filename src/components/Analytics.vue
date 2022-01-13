0dbc66f51f3dbd91202edaa019b8b707ac197bdb
Inffinite <louislaizer4@gmail.com>
2022-01-14 01:00:07 +0300

    :hammer: Analytics stable

--- src/components/Analytics.vue
+++ src/components/Analytics.vue
@@ -1,13 +1,64 @@
 <template>
   <div class="analytics">
     <div class="content">
-        <div class="ntitle">Analytics</div>
+      <div class="ntitle">Analytics</div>
+      <div class="n-subtitle">
+        Vehicle Stats
+      </div>
+
+      <div class="ntab">
+        <div class="table">
+          <div>Car Make</div>
+          <div>Owners</div>
+        </div>
+
+        <div v-for="(make, key, i) in makes" :key="i" class="ndata">
+          <div>{{ key }}</div>
+          <div>{{ make }}</div>
+        </div>
+      </div>
+
+      <div class="ntab">
+        <div class="table">
+          <div>Make & model</div>
+          <div>Owners</div>
+        </div>
+
+        <div v-for="(model, key, i) in models" :key="i" class="ndata">
+          <div>{{ key }}</div>
+          <div>{{ model }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+export default {
+  async mounted(){
+    await axios.get('https://peaceful-beyond-71732.herokuapp.com/analyzeList')
+          .then((res) => {
+            //console.log(res.data)
+            this.makes = res.data.makesAverage[0]
+            this.models = res.data.modelsAverage[0]
+
+            console.log(this.makes)
+
+            console.log(Object.entries(res.data.makesAverage[0]))
+          })
+          .catch((e) => {
+            console.log(e)
+          })
+  },
+
+  data(){
+    return {
+      makes: [],
+      models: []
+    }
+  }
+};
 </script>
 
 <style>
